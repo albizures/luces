2a0ad98933791975b77a1d62e6a032faf9409fc7
Jose Albizures <albizures3601@gmail.com>
2018-03-11 18:53:41 -0600

    add category messages

---
+++ api/src/messages/categories.js
@@ -0,0 +1,22 @@
+export default {
+  added: {
+    message: 'Categoria agregada',
+    description: 'La categoria ha sido agregada correctamente'
+  },
+  addError: {
+    message: 'Error al agregar categoria',
+    description: 'La categoria no pudo ser agregada, intentelo de nuevo'
+  },
+  edited: {
+    message: 'Categoria editada',
+    description: 'La categoria ha sido editada correctamente'
+  },
+  editError: {
+    message: 'Error al agregar categoria',
+    description: 'La categoria no pudo ser agregada, intentelo de nuevo'
+  },
+  getCategories: {
+    message: 'Error',
+    description: 'Ocurrio un error consultando las categorias, por favor recargue la pagina'
+  }
+}
