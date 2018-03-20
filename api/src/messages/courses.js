5cc905702d833a3445dd627fa8dd06fa81610683
Jose Albizures <albizures3601@gmail.com>
2018-03-19 21:33:12 -0600

    add course messages

---
+++ api/src/messages/courses.js
@@ -0,0 +1,22 @@
+export default {
+  added: {
+    message: 'Curso agregado',
+    description: 'El curso ha sido agregada correctamente'
+  },
+  addError: {
+    message: 'Error al agregar curso',
+    description: 'El curso no pudo ser agregada, intentelo de nuevo'
+  },
+  edited: {
+    message: 'Curso editada',
+    description: 'El curso ha sido editada correctamente'
+  },
+  editError: {
+    message: 'Error al agregar categoria',
+    description: 'El curso no pudo ser agregada, intentelo de nuevo'
+  },
+  getCategories: {
+    message: 'Error',
+    description: 'Ocurrio un error consultando los cursos, por favor recargue la pagina'
+  }
+}
