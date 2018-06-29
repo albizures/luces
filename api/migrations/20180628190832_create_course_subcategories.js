bee4ff18fa5ac266b9b016ff4a2d078c814c7069
Jose Albizures <albizures3601@gmail.com>
2018-06-28 19:16:11 -0600

    create course_subcategories table

---
+++ api/migrations/20180628190832_create_course_subcategories.js
@@ -0,0 +1,17 @@
+
+exports.up = (knex) => {
+  return knex.schema.createTable('course_categories', (table) => {
+    table.integer('id_course').unsigned()
+    table.integer('id_subcategory').unsigned()
+    table.boolean('deleted').notNullable().defaultTo(false)
+
+    table.foreign('id_course').references('courses.id')
+    table.foreign('id_subcategory').references('subcategories.id')
+
+    table.timestamps(false, true)
+  })
+}
+
+exports.down = (knex) => {
+  return knex.schema.dropTableIfExists('course_categories')
+}
