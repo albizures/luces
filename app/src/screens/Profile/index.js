175b7a2b6844753301b4193c8fd7b1cce9885338
Jose Albizures <albizures3601@gmail.com>
2018-10-13 09:06:08 -0600

    show favorites in profile screen

--- app/src/screens/Profile/index.js
+++ app/src/screens/Profile/index.js
@@ -11,7 +11,7 @@ import TabIcon from '../../components/TabIcon'
 import Container from '../../components/Container'
 import TopBar from '../../components/TopBar'
 import CircleImage from '../../components/CircleImage'
-import Course from './Course'
+import Course from '../../components/Course'
 
 export default class Profile extends Component {
   static propTypes = {
@@ -29,23 +29,37 @@ export default class Profile extends Component {
   }
 
   state = {
-    loading: true
+    loading: true,
+    courses: []
   }
 
   async componentDidMount () {
-    const { data } = await http.get('login/me')
-    this.setState({
-      ...data,
-      loading: false
-    })
+    try {
+      const { data } = await http.get('login/me')
+      const { data: courses = [] } = await http.get('favorites')
+      console.log(courses)
+
+      this.setState({
+        ...data,
+        courses,
+        loading: false
+      })
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   onBack = () => {
     this.props.navigation.goBack()
   }
 
+  onClickCourse (course) {
+    const { navigation } = this.props
+    navigation.navigate('Course', { course })
+  }
+
   render () {
-    const { name, cover, loading } = this.state
+    const { name, cover, loading, courses } = this.state
     const topBar = (
       <TopBar
         onBack={this.onBack}
@@ -63,9 +77,23 @@ export default class Profile extends Component {
         </View>
         <View style={styles.courses}>
           <Text style={styles.coursesTitle}>Mis cursos</Text>
-          <Course title='Ondas naturales en cabello' icon={require('../../assets/categories/hair.png')} />
+          {/* <Course title='Ondas naturales en cabello' icon={require('../../assets/categories/hair.png')} />
           <Course title='Sombras de ojos nude' icon={require('../../assets/categories/eyes.png')} />
-          <Course title='Curso de uñas' icon={require('../../assets/categories/nail.png')} />
+          <Course title='Curso de uñas' icon={require('../../assets/categories/nail.png')} /> */}
+          {courses.length === 0 && (
+            <Text style={{textAlign: 'center', fontSize: 14, color: colors.whiteTwo}}>Todavía no tienes ningún curso</Text>
+          )}
+          {courses.map((course) => {
+            const {image, name, description, id} = course
+            return (
+              <Course
+                key={id}
+                image={{uri: createUrl(image)}}
+                title={name}
+                description={description}
+                onPress={() => this.onClickCourse(course)} />
+            )
+          })}
         </View>
       </Container>
     )
