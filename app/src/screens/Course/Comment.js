eab248919e887054b3a219d4a213fcb96b0234a2
Jose Albizures <albizures3601@gmail.com>
2019-03-10 15:38:41 -0600

    Create Comments component

---
+++ app/src/screens/Course/Comment.js
@@ -0,0 +1,151 @@
+import React, { Component } from 'react'
+import { View, Image, Text, StyleSheet } from 'react-native'
+import PropTypes from 'prop-types'
+import dayjs from 'dayjs'
+
+import createUrl from '../../utils/createUrl'
+import colors from '../../utils/colors'
+import http from '../../utils/http'
+import { withUser } from '../../components/UserContext'
+import Heart from '../../components/Heart'
+import { withCourseContext, CourseContextShape } from './CourseContext'
+
+class Comment extends Component {
+  static propTypes = {
+    comment: PropTypes.object.isRequired,
+    user: PropTypes.object,
+    courseContext: PropTypes.shape(CourseContextShape).isRequired,
+  }
+
+  constructor (props) {
+    super(props)
+
+    const { comment } = this.props
+    const { liked, likes } = comment
+
+    this.state = {
+      liked,
+      likes,
+    }
+  }
+
+  toggleLike = async () => {
+    const { comment, user } = this.props
+    const { userRequiredAlert } = this.props.courseContext
+    const { id } = comment
+    const { liked } = this.state
+
+    if (!user) {
+      return userRequiredAlert({
+        title: '¿Quieres ser parte de la comunidad?',
+        subtitle: 'Crea tu cuenta gratuita de Luces Beautiful para poder comentar.',
+      })
+    }
+
+    try {
+      if (liked) {
+        const { data: { likes } } = await http.del(`comments/${id}/like`)
+        this.setState({ likes, liked: !liked })
+      } else {
+        const { data: { likes } } = await http.post(`comments/${id}/like`)
+        this.setState({ likes, liked: !liked })
+      }
+    } catch (error) {
+      alert('No se puedo guardar la reaccion')
+      console.log('Comments', error)
+    }
+  }
+
+  render () {
+    const { comment } = this.props
+    const { comment: text, userName, date, cover } = comment
+    const { liked, likes } = this.state
+
+    const source = cover
+      ? { uri: createUrl(cover) }
+      : require('../../assets/account.png')
+
+    return (
+      <View style={styles.comment}>
+        <Image style={styles.photo} source={source} />
+        <View style={styles.commentContainer}>
+          <Text style={styles.userName}>{userName}</Text>
+          <Text style={styles.text}>{text}</Text>
+          <View style={styles.commentBottom}>
+            <View style={styles.dateContainer}>
+              <Text style={styles.date}>{dayjs(date).format('D MMMM, YYYY')}</Text>
+            </View>
+            <View style={styles.likesContainer}>
+              <Text style={styles.likesCount} >{likes || 0} me gusta</Text>
+              <Heart style={styles.like} active={!!liked} onPress={this.toggleLike} />
+            </View>
+          </View>
+        </View>
+      </View>
+    )
+  }
+}
+
+export default withCourseContext(withUser(Comment))
+
+const styles = StyleSheet.create({
+  like: {
+    width: 14,
+    height: 14,
+  },
+  likesCount: {
+    color: colors.whiteTwo,
+    fontWeight: 'bold',
+    fontSize: 10,
+  },
+  likesContainer: {
+    flex: 1,
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+  },
+  date: {
+    color: colors.slateGrey,
+    fontSize: 10,
+  },
+  dateContainer: {
+    paddingRight: 10,
+    marginRight: 10,
+    borderRightColor: colors.slateGrey,
+    borderRightWidth: 1,
+  },
+  commentBottom: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 12,
+    fontWeight: '500',
+    color: colors.whiteTwo,
+    marginBottom: 6,
+  },
+  userName: {
+    fontSize: 14,
+    color: colors.darkTan,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  comment: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+    paddingTop: 20,
+    paddingBottom: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: colors.gunmetal,
+  },
+  photo: {
+    height: 40,
+    width: 40,
+    borderRadius: 20,
+    marginRight: 10,
+  },
+  commentContainer: {
+    flex: 1,
+  },
+})
--- app/src/screens/Course/Comments.js
+++ app/src/screens/Course/Comments.js
@@ -1,23 +1,14 @@
 import React, { PureComponent } from 'react'
-import { View, Text, TextInput, Image } from 'react-native'
+import { View, Text, TextInput } from 'react-native'
 import PropTypes from 'prop-types'
-import dayjs from 'dayjs'
 
-import Heart from '../../components/Heart'
-import { withUser } from '../../components/UserContext'
-import createUrl from '../../utils/createUrl'
 import colors from '../../utils/colors'
-import http from '../../utils/http'
+import Comment from './Comment'
 import { withCourseContext, CourseContextShape } from './CourseContext'
 
 class Comments extends PureComponent {
   static propTypes = {
-    user: PropTypes.object,
     courseContext: PropTypes.shape(CourseContextShape).isRequired,
-  }
-
-  state = {
-    text: '',
   }
 
   // state = {
@@ -49,76 +40,15 @@ class Comments extends PureComponent {
   //   ]
   // }
 
-  setCommentState (comment, likes, liked, index) {
-    const { comments } = this.state
-    this.setState({
-      comments: Object.assign(
-        comments.concat(),
-        { [index]: Object.assign({}, comment, { likes: likes, liked: liked }) }
-      ),
-    })
-  }
-
   onFocus = (evt) => {
     const { focusTextInput } = this.props.courseContext
 
     focusTextInput()
   }
 
-  async toggleLike (comment, index) {
-    const { user } = this.props
-    const { userRequiredAlert } = this.props.courseContext
-    const { id, liked } = comment
-
-    if (!user) {
-      return userRequiredAlert({
-        title: '¿Quieres ser parte de la comunidad?',
-        subtitle: 'Crea tu cuenta gratuita de Luces Beautiful para poder comentar.',
-      })
-    }
-
-    try {
-      if (liked) {
-        const { data: { likes } } = await http.del(`comments/${id}/like`)
-        this.setCommentState(comment, likes, !liked, index)
-      } else {
-        const { data: { likes } } = await http.post(`comments/${id}/like`)
-        this.setCommentState(comment, likes, !liked, index)
-      }
-    } catch (error) {
-      alert('No se puedo guardar la reaccion')
-      console.log('Comments', error)
-    }
-  }
-
-  getComment = (comment, index) => {
-    const { comment: text, userName, liked, date, likes, id, cover } = comment
-
-    const source = cover
-      ? { uri: createUrl(cover) }
-      : require('../../assets/account.png')
-    return (
-      <View key={id} style={styles.comment}>
-        <Image style={styles.photo} source={source} />
-        <View style={styles.commentContainer}>
-          <Text style={styles.userName}>{userName}</Text>
-          <Text style={styles.text}>{text}</Text>
-          <View style={styles.commentBottom}>
-            <View style={styles.dateContainer}>
-              <Text style={styles.date}>{dayjs(date).format('D MMMM, YYYY')}</Text>
-            </View>
-            <View style={styles.likesContainer}>
-              <Text style={styles.likesCount} >{likes || 0} me gusta</Text>
-              <Heart style={styles.like} active={!!liked} onPress={() => this.toggleLike(comment, index)} />
-            </View>
-          </View>
-        </View>
-      </View>
-    )
-  }
-
   render () {
     const { comments } = this.props.courseContext
+
     return (
       <View style={{ flex: 1 }}>
         <View style={styles.inputContainer}>
@@ -134,78 +64,20 @@ class Comments extends PureComponent {
             // numberOfLines={4}
             style={styles.input} />
         </View>
-        {comments.map(this.getComment)}
+        {comments.map((comment) => (
+          <Comment key={comment.id} comment={comment} />
+        ))}
       </View>
     )
   }
 }
 
 const styles = {
-  likesContainer: {
-    flex: 1,
-    justifyContent: 'space-between',
-    flexDirection: 'row',
-  },
-  dateContainer: {
-    paddingRight: 10,
-    marginRight: 10,
-    borderRightColor: colors.slateGrey,
-    borderRightWidth: 1,
-  },
   commentsCount: {
     fontSize: 14,
     marginBottom: 18,
     fontWeight: 'bold',
     color: colors.whiteTwo,
-  },
-  likesCount: {
-    color: colors.whiteTwo,
-    fontWeight: 'bold',
-    fontSize: 10,
-  },
-  date: {
-    color: colors.slateGrey,
-    fontSize: 10,
-  },
-  like: {
-    width: 14,
-    height: 14,
-  },
-  userName: {
-    fontSize: 14,
-    color: colors.darkTan,
-    fontWeight: 'bold',
-    marginBottom: 10,
-  },
-  text: {
-    fontSize: 12,
-    fontWeight: '500',
-    color: colors.whiteTwo,
-    marginBottom: 6,
-  },
-  commentBottom: {
-    flexDirection: 'row',
-    // justifyContent: 'center',
-    alignItems: 'center',
-  },
-  commentContainer: {
-    flex: 1,
-  },
-  photo: {
-    height: 40,
-    width: 40,
-    borderRadius: 20,
-    marginRight: 10,
-  },
-  comment: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    paddingHorizontal: 20,
-    paddingTop: 20,
-    paddingBottom: 10,
-    borderBottomWidth: 1,
-    borderBottomColor: colors.gunmetal,
   },
   inputContainer: {
     marginHorizontal: 20,
@@ -221,4 +93,4 @@ const styles = {
   },
 }
 
-export default withCourseContext(withUser(Comments))
+export default withCourseContext(Comments)
